d592a7ab0f3f122f05ab48aa37bf6078f8873294
Maria <mariadybdahl@hotmail.dk>
2025-09-04 15:28:52 +0200

    Update Program.cs

--- BiteLogRESTAPI/Program.cs
+++ BiteLogRESTAPI/Program.cs
@@ -24,8 +24,6 @@ builder.Services.AddCors(options =>
               .AllowAnyMethod(); // Tillad alle HTTP-metoder (GET, POST, PUT, DELETE osv.)
     });
 });
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
